9f65b0f51ca9a7a7df905bbccbacea1c884bbe30
Michał Ciechański <miciechanski@gmail.com>
2016-01-22 15:01:34 +0100

    Working over position file

--- src/com/company/Position.java
+++ src/com/company/Position.java
@@ -16,7 +16,7 @@ public class Position {
 
     public Optional<Position> getCrossedBorder (Cell currentCell,Position currentPosition, DirectionCoefficient currentDirectionCoefficient, Position predictedPosition){
 
-        Position coordinatesOfCrossing;
+        ArrayList<Position> coordinatesOfCrossing;
 
         if ((wgranicyKomorki(currentCell, currentPosition)== true && wgranicyKomorki(currentCell, predictedPosition)==true)||
                 (wgranicyKomorki(currentCell, currentPosition)== false && wgranicyKomorki(currentCell, predictedPosition)==false) ){
@@ -26,27 +26,27 @@ public class Position {
 
             if (predictedPosition.x < currentCell.xMin) {
                 Double wsp = (currentPosition.x - currentCell.xMin) / currentDirectionCoefficient.x;
-                coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
             }
             else if (predictedPosition.x > currentCell.xMax){
                 Double wsp = (currentPosition.x - currentCell.xMax) / currentDirectionCoefficient.x;
-                 coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                 coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
             }
             else if (predictedPosition.y < currentCell.yMin) {
                 Double wsp = (currentPosition.y - currentCell.yMin) / currentDirectionCoefficient.y;
-                coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
             }
             else if (predictedPosition.y > currentCell.yMax) {
                 Double wsp = (currentPosition.y - currentCell.yMax) / currentDirectionCoefficient.y;
-                coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
             }
             else if (predictedPosition.z < currentCell.zMin) {
                 Double wsp = (currentPosition.z - currentCell.zMin) / currentDirectionCoefficient.z;
-                coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                coordinatesOfCrossing.add(getCrossingPosition(currentPosition, wsp, currentDirectionCoefficient));
             }
             else if (predictedPosition.z > currentCell.zMax) {
                 Double wsp = (currentPosition.z - currentCell.zMax) / currentDirectionCoefficient.z;
-                coordinatesOfCrossing = getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient);
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
             }
         }
         return coordinatesOfCrossing;
@@ -58,6 +58,15 @@ public class Position {
 
     private Position getCrossingPosition (Position currentPosition, Double wsp, DirectionCoefficient currentDirectionCoefficient){
         return  new Position(currentPosition.x - wsp * currentDirectionCoefficient.x, currentPosition.x - wsp * currentDirectionCoefficient.y, currentPosition.z - wsp * currentDirectionCoefficient.z);
+    }
+
+    private Double getDistanceBetweenTwoPositions (Position position1, Position position2){
+        return Math.sqrt(Math.pow(position1.x - position2.x, 2) + Math.pow(position1.y - position2.y, 2) + Math.pow(position1.z - position2.z, 2));
+    }
+
+    private Position getClosestCrossBorderPoint( ArrayList <Position> coordinatesOfCrossing){
+
+        return ;
     }
 
     // jeśli jeden jest a drugi nie, to robimygeomatryczną magię i wyznaczamy punkt przecięcia prostej między dwoma position a granicą fukcji i go zwracamy
