29078dc71baaabaa0ab006ab5ff47194e847b4c8
Michał Ciechański <miciechanski@gmail.com>
2016-01-22 17:56:19 +0100

    Working over position file

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class Cell {
 
     public int xMin;
@@ -18,8 +21,51 @@ public class Cell {
         this.zMax = zMax;
     }
 
-    public static Boolean wgranicyKomorki(Cell cell, Position position) {
-        return position.x > cell.xMin && position.x < cell.xMax && position.y > cell.yMin && position.y < cell.yMax && position.z > cell.zMin && position.z < cell.zMax;
+    public Optional<Position> getCrossedBorder (Cell currentCell,Position currentPosition, DirectionCoefficient currentDirectionCoefficient, Position predictedPosition){
+
+        ArrayList<Position> coordinatesOfCrossing;
+
+        if ((wGranicy(currentPosition) && wGranicy(predictedPosition))||
+                (!wGranicy(currentPosition) && !wGranicy(predictedPosition))){
+            return Optional.empty();
+        }
+        else if ((wGranicy(currentPosition) && !wGranicy(predictedPosition))){
+
+            if (predictedPosition.x < currentCell.xMin) {
+                Double wsp = (currentPosition.x - currentCell.xMin) / currentDirectionCoefficient.x;
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
+            }
+            else if (predictedPosition.x > currentCell.xMax){
+                Double wsp = (currentPosition.x - currentCell.xMax) / currentDirectionCoefficient.x;
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
+            }
+            else if (predictedPosition.y < currentCell.yMin) {
+                Double wsp = (currentPosition.y - currentCell.yMin) / currentDirectionCoefficient.y;
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
+            }
+            else if (predictedPosition.y > currentCell.yMax) {
+                Double wsp = (currentPosition.y - currentCell.yMax) / currentDirectionCoefficient.y;
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
+            }
+            else if (predictedPosition.z < currentCell.zMin) {
+                Double wsp = (currentPosition.z - currentCell.zMin) / currentDirectionCoefficient.z;
+                coordinatesOfCrossing.add(getCrossingPosition(currentPosition, wsp, currentDirectionCoefficient));
+            }
+            else if (predictedPosition.z > currentCell.zMax) {
+                Double wsp = (currentPosition.z - currentCell.zMax) / currentDirectionCoefficient.z;
+                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
+            }
+        }
+        return coordinatesOfCrossing.stream().min();
+    }
+
+    public  Boolean wGranicy(Position position) {
+        return position.x > xMin && position.x < xMax && position.y > yMin && position.y < yMax && position.z > zMin && position.z < zMax;
+    }
+
+
+    public Position getCrossingPosition (Position currentPosition, Double wsp, DirectionCoefficient currentDirectionCoefficient){
+        return  new Position(currentPosition.x - wsp * currentDirectionCoefficient.x, currentPosition.x - wsp * currentDirectionCoefficient.y, currentPosition.z - wsp * currentDirectionCoefficient.z);
     }
 
 
--- src/com/company/Position.java
+++ src/com/company/Position.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.Collections;
 
 public class Position {
     Double x;
@@ -14,67 +14,18 @@ public class Position {
         this.z = z;
     }
 
-    public Optional<Position> getCrossedBorder (Cell currentCell,Position currentPosition, DirectionCoefficient currentDirectionCoefficient, Position predictedPosition){
 
-        ArrayList<Position> coordinatesOfCrossing;
 
-        if ((wgranicyKomorki(currentCell, currentPosition)== true && wgranicyKomorki(currentCell, predictedPosition)==true)||
-                (wgranicyKomorki(currentCell, currentPosition)== false && wgranicyKomorki(currentCell, predictedPosition)==false) ){
-            return Optional.empty();
-        }
-        else if ((wgranicyKomorki(currentCell, currentPosition)== true && wgranicyKomorki(currentCell, predictedPosition)==false)){
-
-            if (predictedPosition.x < currentCell.xMin) {
-                Double wsp = (currentPosition.x - currentCell.xMin) / currentDirectionCoefficient.x;
-                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
-            }
-            else if (predictedPosition.x > currentCell.xMax){
-                Double wsp = (currentPosition.x - currentCell.xMax) / currentDirectionCoefficient.x;
-                 coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
-            }
-            else if (predictedPosition.y < currentCell.yMin) {
-                Double wsp = (currentPosition.y - currentCell.yMin) / currentDirectionCoefficient.y;
-                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
-            }
-            else if (predictedPosition.y > currentCell.yMax) {
-                Double wsp = (currentPosition.y - currentCell.yMax) / currentDirectionCoefficient.y;
-                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
-            }
-            else if (predictedPosition.z < currentCell.zMin) {
-                Double wsp = (currentPosition.z - currentCell.zMin) / currentDirectionCoefficient.z;
-                coordinatesOfCrossing.add(getCrossingPosition(currentPosition, wsp, currentDirectionCoefficient));
-            }
-            else if (predictedPosition.z > currentCell.zMax) {
-                Double wsp = (currentPosition.z - currentCell.zMax) / currentDirectionCoefficient.z;
-                coordinatesOfCrossing.add(getCrossingPosition (currentPosition, wsp, currentDirectionCoefficient));
-            }
-        }
-        return coordinatesOfCrossing;
+    public Position getClosestCrossBorderPoint( ArrayList <Position> coordinatesOfCrossing, Position currentPosition){
+        //TODO Chcę żeby to zwróciło  position o najmniejszej odległości
+        coordinatesOfCrossing.forEach(Position -> getDistanceBetweenTwoPositions(currentPosition));
+            return getClosestCrossBorderPoint min(getDistanceBetweenTwoPositions(currentPosition));
     }
 
-    private  Boolean wgranicyKomorki(Cell cell, Position position) {
-        return position.x > cell.xMin && position.x < cell.xMax && position.y > cell.yMin && position.y < cell.yMax && position.z > cell.zMin && position.z < cell.zMax;
-    }
-
-    private Position getCrossingPosition (Position currentPosition, Double wsp, DirectionCoefficient currentDirectionCoefficient){
-        return  new Position(currentPosition.x - wsp * currentDirectionCoefficient.x, currentPosition.x - wsp * currentDirectionCoefficient.y, currentPosition.z - wsp * currentDirectionCoefficient.z);
-    }
-
-    private Position getClosestCrossBorderPoint( ArrayList <Position> coordinatesOfCrossing){
-
-        for (coordinatesOfCrossing : Position){
-
-        }
-
-        return ;
-    }
-
-    private Double getDistanceBetweenTwoPositions (Position position1, Position position2){
-        return Math.sqrt(Math.pow(position1.x - position2.x, 2) + Math.pow(position1.y - position2.y, 2) + Math.pow(position1.z - position2.z, 2));
+    public Double getDistanceBetweenTwoPositions (Position position2){
+        return Math.sqrt(Math.pow(this.x - position2.x, 2) + Math.pow(this.y - position2.y, 2) + Math.pow(this.z - position2.z, 2));
     }
 
 
-
-    // jeśli jeden jest a drugi nie, to robimygeomatryczną magię i wyznaczamy punkt przecięcia prostej między dwoma position a granicą fukcji i go zwracamy
 
 }
