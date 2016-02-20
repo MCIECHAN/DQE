38b0b5272a891e2042a875f2560d764370062cc8
Michał Ciechański <miciechanski@gmail.com>
2016-02-20 15:07:24 +0100

    testing results.

--- src/com/company/PartialLSFFunction.java
+++ src/com/company/PartialLSFFunction.java
@@ -4,4 +4,7 @@ package com.company;
  * Created by ciechan on 2016-02-20.
  */
 public class PartialLSFFunction {
+
+    private Double propablityOfDetection;
+    private
 }
---
+++ src/com/company/PositionOfDetection.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by ciechan on 2016-02-20.
+ */
+public class PositionOfDetection {
+    int X;
+    int Y;
+
+    public PositionOfDetection (int newX, int newY){
+    this.X = newX;
+        this.Y = newY;
+    }
+
+}
