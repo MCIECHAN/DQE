1e14471cec5c5500bcb05e9d2e4b202c14a91b0a
Michał Ciechański <miciechanski@gmail.com>
2016-02-20 15:47:36 +0100

    developing......

--- src/com/company/CollectorOfLSFFunctions.java
+++ src/com/company/CollectorOfLSFFunctions.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by ciechan on 2016-02-20.
  */
 public class CollectorOfLSFFunctions {
+
+    private ArrayList <PartialLSFFunction> listOfPartialLSFFunctions;
+
+    CollectorOfLSFFunctions (){
+
+    }
 
 }
--- src/com/company/PartialLSFFunction.java
+++ src/com/company/PartialLSFFunction.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by ciechan on 2016-02-20.
  */
 public class PartialLSFFunction {
 
+    private Double positonZ;
     private Double propablityOfDetection;
-    private
+    private ArrayList<PositionOfDetection> listOfPositionsOfDetection;
+
+    PartialLSFFunction (){
+
+    }
+
 }
